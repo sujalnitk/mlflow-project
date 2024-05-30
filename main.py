e28df3a70da34db40f59ab9e39b81a88e63d0f28
GhostBYte <sksujaliitg@gmail.com>
2024-05-30 17:24:24 +0530

    exception logger and utils addeed

---
+++ main.py
@@ -0,0 +1,4 @@
+from mlproject import logger
+
+
+logger.info("Welcome to our custom logging")
--- template.py
+++ template.py
@@ -26,7 +26,8 @@ list_of_files = [
     "requirements.txt",
     "setup.py",
     "research/trials.ipynb",
-    "templates/index.html"
+    "templates/index.html",
+    "test.py"
 ]
 
 for filepath in list_of_files:
