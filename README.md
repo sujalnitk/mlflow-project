895e646100033e6328e139effaa217d80c2f9055
GhostBYte <sksujaliitg@gmail.com>
2024-06-01 14:49:43 +0530

    data ingestion completed

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # mlflow-project
+
+## Workflows
+
+1. Update config.yaml
+2. Update schema.yaml
+3. Update params.yaml
+4. Update the entity 
+5. Update the Configuration mangager in src config
+6. Update the components 
+7. Update the pipeline
+8. Update main.py
+9. Update app.py
--- main.py
+++ main.py
@@ -1,4 +1,13 @@
 from mlproject import logger
+from mlproject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 
+STAGE_NAME = 'Data Ingestion Stage'
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_ingestion = DataIngestionTrainingPipeline()
+   data_ingestion.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
 
-logger.info("Welcome to our custom logging")
